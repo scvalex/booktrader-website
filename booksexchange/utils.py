783f8f3a7977301e395cafb3665a0120b83546fa
Francesco <f@mazzo.li>
2011-05-27 17:54:13 +0200

    Added function to send emails.

--- booksexchange/utils.py
+++ booksexchange/utils.py
@@ -4,6 +4,11 @@ from repoze.folder           import Folder
 from repoze.catalog.catalog  import Catalog
 from repoze.catalog.document import DocumentMap
 
+import smtplib
+from email import Header
+from email.mime.text import MIMEText
+
+
 class IndexFolder(Folder):
     def __init__(self, **kwargs):
         super(IndexFolder, self).__init__()
@@ -36,4 +41,53 @@ class IndexFolder(Folder):
     def update(self, obj):
         self._catalog.reindex_doc(self._docmap.docid_for_address(resource_path(obj)),
                                   obj)
+
+
+def send_email(body, subject, recipients, settings, sender=None):
+    """Send an email.
+
+    All arguments should be Unicode strings (plain ASCII works as well).
+
+    Only the real name part of sender and recipient addresses may contain
+    non-ASCII characters.
+
+    The charset of the email will be the first one out of US-ASCII, ISO-8859-1
+    and UTF-8 that can represent all the characters occurring in the email.
+
+    Thanks to http://mg.pov.lt/blog/unicode-emails-in-python for the
+    encoding tips.
+    """
+    
+    # Fallback to default sender
+    if not sender:
+        sender = settings['smtp_email']
+    
+    # Header class is smart enough to try US-ASCII, then the charset we
+    # provide, then fall back to UTF-8.
+    header_charset = 'ISO-8859-1'
+
+    # We must choose the body charset manually
+    for body_charset in 'US-ASCII', 'ISO-8859-1', 'UTF-8':
+        try:
+            body.encode(body_charset)
+        except UnicodeError:
+            pass
+        else:
+            break
+
+    # Make sure email addresses do not contain non-ASCII characters
+    sender = sender.encode('ascii')
+    recipients = [recipient.encode('ascii') for recipient in recipients]
+
+    # Sends the email through SSL
+    msg = MIMEText(body.encode(body_charset), 'plain', body_charset)
+    msg['Subject'] = subject.encode('ascii')
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+
+    server = smtplib.SMTP_SSL(settings['smtp_server'], settings['smtp_port'])
+    server.login(settings['smtp_username'], settings['smtp_password'])
+    server.sendmail(sender, recipients, msg.as_string())
+    
+    server.quit()
 
